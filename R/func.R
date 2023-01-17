62b778a11c07a5d77e4e6004f8733ec109e3cb9e
Erin Macartney <z3439723@ad.unsw.edu.au>
2023-01-17 14:05:08 +1100

    extracting more data, running functions, creating a column with all effect sizes

--- R/func.R
+++ R/func.R
@@ -189,3 +189,20 @@ p_vals <- function(p_val, n1, n2, reverse = FALSE){ # n2 = higher/larger group
 #p_vals(0.004, 20, 20, reverse = TRUE)
 
 
+p_vals_b <- function(p_val, N, reverse = FALSE){ # n2 = higher/larger group
+  n12 <- N
+  n1 <- N/2
+  n2 <- N/2
+  n12 <- n1 + n2
+  #h <- n12/n1 + n12/n2
+  p <- n1/n12 # prop for n1
+  q <- n2/n12 # prop for n2
+  t <- qt(1 - p_val, n12 - 2)
+  #t <- est/se
+  r_pb <- t/sqrt(t^2 + n12 -2)
+  
+  r_b <- r_pb*(sqrt(p*q)/dnorm(qnorm(p)))
+  if(reverse == TRUE){
+    r_b = r_b*(-1)}
+  r_b
+}
